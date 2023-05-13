b5682dd83fc79331ef6331cb8b1af38a6cdab8d7
Maksym Pozhydaiev <pogidaevmo@gmail.com>
2023-05-13 19:57:00 +0000

    add get-video-duration

--- src/main/video/VideoFile.ts
+++ src/main/video/VideoFile.ts
@@ -1,8 +1,19 @@
 import ffmpeg from './provider';
 import InputFile from '../file/InputFile';
+// @ts-ignore
+import { getVideoDurationInSeconds } from 'get-video-duration';
+
 
 export default class VideoFile extends InputFile {
   async handle(reply: any): Promise<void> {
+    let durationVal: number | undefined;
+    try {
+      const duration = await getVideoDurationInSeconds(this.srcFile);
+      durationVal = Number(duration);
+    } catch (err) {
+      console.log(err);
+    }
+
     return new Promise((resolve, reject) => {
       ffmpeg(this.srcFile)
         .format('avi')
@@ -14,14 +25,14 @@ export default class VideoFile extends InputFile {
         .on('end', () => resolve())
         .on('error', (err) => reject(err))
         .on('progress', (progress) => {
-          const { percent } = progress;
+          let { percent } = progress;
 
-          // if (!percent && progress.timemark) {
-          //   const [hours, min, sec] = progress.timemark.split(':');
-          //   const currentHandledTime =
-          //     Number(sec) + Number(min * 60) + Number(hours * 360);
-          //   percent = currentHandledTime / durationVal;
-          // }
+          if (!percent && progress.timemark && durationVal) {
+            const [hours, min, sec] = progress.timemark.split(':');
+            const currentHandledTime =
+              Number(sec) + Number(min * 60) + Number(hours * 360);
+            percent = currentHandledTime / durationVal;
+          }
 
           if (percent) {
             const formatted = percent > 1 ? 100 : Math.round(percent * 100);
