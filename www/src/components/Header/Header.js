64123992f92bf24b6ce56bf2f82745251896fe25
max-im <pogidaevmo@gmail.com>
2024-12-27 00:30:10 +0200

    add sitemap

--- www/src/components/Header/Header.js
+++ www/src/components/Header/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
       <div className="container-xl mx-auto flex justify-between items-center py-4 px-6">
         <img src={logo} alt="MediaCompressor" className={`${styles.logo} mr-2`} />
         <div>
-          <a href="https://max-im.github.io/media-compressor/mediaCompressor.zip" className="bg-black hover:bg-gray-700 animate-pulse text-white px-4 py-2 mr-12 rounded-md " download>Download</a>
+          <a href="https://max-im.github.io/media-compressor/media-compressor-0.3.1" className="bg-black hover:bg-gray-700 animate-pulse text-white px-4 py-2 mr-12 rounded-md " download>Download</a>
           <GithubCorner href="https://github.com/max-im/media-compressor" />
         </div>
       </div>
