03c5ef370846e4eb1ca742a0fa64232e571fee91
max-im <pogidaevmo@gmail.com>
2024-12-30 00:25:15 +0200

    DEV: ADD: init events

--- app/src/components/Header/Header.tsx
+++ app/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import packageJson from '../../../package.json';
+import logo from '../../assets/logo.png';
 import styles from './Header.module.css';
 
 export default function Header() {
@@ -12,7 +13,10 @@ export default function Header() {
     }, [])
   return (
     <header className={`${styles.bg} block`}>
-        <h1>Media Compressor</h1>
+        <div className="flex items-center">
+          <img src={logo} className="mr-2" alt="logo" width="36" height="36" />
+          <h1>Media Compressor</h1>
+        </div>
         <h4>{description}</h4>
         <p>Version: {version}</p>
     </header>
--- app/src/components/UploadMedia/UploadMedia.tsx
+++ app/src/components/UploadMedia/UploadMedia.tsx
@@ -1,3 +1,4 @@
+import folder from '../../assets/folder.svg';
 import styles from './UploadMedia.module.css';
 
 export default function UploadMedia() {
@@ -9,7 +10,8 @@ export default function UploadMedia() {
   return (
     <div className={`${styles.bg} block grow-1`}>
         <h3>Upload Media</h3>
-        <button className={styles.btn} onClick={handleUpload}>
+        <button className="bg-black hover:bg-gray-700 text-white border-none text-base py-2 px-3 cursor-pointer flex items-center" onClick={handleUpload}>
+          <img src={folder} alt="icon" width="24" height="24" className="mr-1" />
           Select Folder
         </button>
     </div>
